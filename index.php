71ac35702606d4a4932d4a5a99a8fc46cacbae81
Tera-CZYD <81411991+Tera-CZYD@users.noreply.github.com>
2023-08-31 09:45:44 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,10 +7,21 @@
  * For full copyright and license information, please see the LICENSE.txt
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @copyright     Copyright (c) Cake Software Foundation, Inc.
  * @link          https://cakephp.org CakePHP(tm) Project
- * @since         0.10.0
+ * @since         0.2.9
  * @license       https://opensource.org/licenses/mit-license.php MIT License
  */
 
-require 'webroot' . DIRECTORY_SEPARATOR . 'index.php';
+// For built-in server
+if (php_sapi_name() === 'cli-server') {
+    $_SERVER['PHP_SELF'] = '/' . basename(__FILE__);
+
+    $url = parse_url(urldecode($_SERVER['REQUEST_URI']));
+    $file = __DIR__ . $url['path'];
+    if (strpos($url['path'], '..') === false && strpos($url['path'], '.') !== false && is_file($file)) {
+        return false;
+    }
+}
+
+require dirname(__DIR__) . '/webroot/index.php';
